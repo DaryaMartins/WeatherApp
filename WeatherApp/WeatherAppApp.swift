445775d22d9a0cfddc7cbc84029ad5404a036a64
Darya Martynenko <darya.s.martins@gmail.com>
2023-11-07 13:17:56 +0100

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Darya Martynenko on 07.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
